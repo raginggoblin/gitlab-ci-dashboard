8b651f04f7f26b32f5c87baaea4d53b622eaee05
Rijk Oosterhoff <rijk.oosterhoff@rws.nl>
2026-03-24 15:01:24 +0100

    Add build steps

--- build.sh
+++ build.sh
@@ -7,18 +7,33 @@ OPENAPI_OUTPUT_DIR="./backend/target/generated-sources/openapi"
 OPENAPI_IGNORE_SOURCE="./openapi/.openapi-generator-ignore"
 OPENAPI_IGNORE_TARGET="./backend/target/generated-sources/openapi/"
 
-# OpenApi
+function echoBuildStep() {
+  echo "#############################################################################################################"
+  echo "#######################################   ${buildstep}   ######################################################"
+  echo "#############################################################################################################"
+}
+
+buildstep='Backend clean'
+echoBuildStep
+cd backend
+mvn clean
+cd ..
+
+buildstep='Openapi generate'
+echoBuildStep
 rm -rf ${OPENAPI_OUTPUT_DIR}
 mkdir -p ${OPENAPI_IGNORE_TARGET}
 cp ${OPENAPI_IGNORE_SOURCE} ${OPENAPI_IGNORE_TARGET}
 docker run --rm -i -t -v $(pwd):$(pwd) -w $(pwd) -u $(id -u) ${DOCKER_IMAGE}:${OPENAPI_VERSION} batch --clean --fail-fast --verbose openapi/config/*
 
-# Backend
+buildstep='Backend build'
+echoBuildStep
 cd backend
-mvn -U -DskipTests -Dmaven.test.skip clean compile
+mvn -U -DskipTests -Dmaven.test.skip compile
 cd ..
 
-# Frontend
+buildstep='Frontend'
+echoBuildStep
 cd frontend
 npm ci
 npm run build
